8f71716faff1b9240eab8c4d4272c77af6407832
Davisulaiman <davisulaiman1@gmail.com>
2024-11-19 21:47:30 +0700

    modal hapus

--- resources/views/penilaian_praktikum/index.blade.php
+++ resources/views/penilaian_praktikum/index.blade.php
@@ -62,15 +62,40 @@
                                         @endif
 
                                         <!-- Tombol Hapus (Dapat Diakses Semua Role) -->
-                                        <form action="{{ route('penilaian_praktikum.destroy', $penilaian->id) }}"
-                                            method="POST" style="display:inline;">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn btn-danger btn-sm"
-                                                onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?')">
-                                                <i class="fas fa-trash"></i> Hapus
-                                            </button>
-                                        </form>
+                                        <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal"
+                                            data-bs-target="#deleteModal{{ $penilaian->id }}">
+                                            <i class="fas fa-trash"></i> Hapus
+                                        </button>
+
+                                        <!-- Modal Konfirmasi Hapus -->
+                                        <div class="modal fade" id="deleteModal{{ $penilaian->id }}" tabindex="-1"
+                                            aria-labelledby="deleteModalLabel{{ $penilaian->id }}" aria-hidden="true">
+                                            <div class="modal-dialog">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title" id="deleteModalLabel{{ $penilaian->id }}">
+                                                            Konfirmasi Hapus</h5>
+                                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                            aria-label="Close"></button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        Apakah kamu yakin ingin menghapus penilaian praktikum
+                                                        <strong>{{ $penilaian->mataKuliahPraktikum->nama_mata_kuliah }}</strong>?
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary"
+                                                            data-bs-dismiss="modal">Batal</button>
+                                                        <form
+                                                            action="{{ route('penilaian_praktikum.destroy', $penilaian->id) }}"
+                                                            method="POST" style="display:inline;">
+                                                            @csrf
+                                                            @method('DELETE')
+                                                            <button type="submit" class="btn btn-danger">Hapus</button>
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </td>
                                 </tr>
                             @endforeach
